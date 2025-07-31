758d6d3b167b588b871f5087a8acdf8f4e9c4458
Rhys Lloyd <krakow20@gmail.com>
2025-07-31 01:54:38 -0700

    words map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+#[derive(Debug,Clone)]
+struct LetterMap{
+	next_letter:[Option<Box<LetterMap>>;27],
+}
+impl LetterMap{
+	fn new()->Self{
+		LetterMap{
+			next_letter:[None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
+		}
+	}
+}
 fn main() {
 	const WORDS:&str=include_str!("words.txt");
+	let mut word_map=LetterMap::new();
+
+	let mut count=0;
+	let mut word_count=0;
+	'outer: for word in WORDS.lines(){
+		let mut letter_map=&mut word_map.next_letter;
+		for &letter in word.as_bytes(){
+			if letter<b'a'||b'z'<letter{
+				continue 'outer;
+			}
+			let index=letter-b'a';
+			if letter_map[index as usize].is_none(){
+				letter_map[index as usize].replace(Box::new(LetterMap::new()));
+				count+=1;
+			}
+			letter_map=match &mut letter_map[index as usize]{
+				Some(thing)=>&mut thing.next_letter,
+				None=>{
+					panic!("Next letter map just inserted does not exist");
+				}
+			};
+		}
+		word_count+=1;
+	}
 }
